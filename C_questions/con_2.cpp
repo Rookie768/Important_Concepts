3aac8a841c615af708b7741f6c3abec22f033055
Achyut <achyutsingh6@gmail.com>
2024-09-14 20:13:59 +0530

    fopen() ques

---
+++ C_questions/con_2.cpp
@@ -0,0 +1,14 @@
+
+// What is the purpose of "rb" in fopen() function used below in the code?
+// FILE *fp;
+// fp = fopen("source.txt", "rb");
+
+// 1. open "source.txt" in binary mode for reading
+// 2.open "source.txt" in binary mode for reading and writing
+// 3. Create a new file "source.txt" for reading and writing
+// 4. None of above
+
+
+// Answer: Option 1
+// Explanation:
+// The file source.txt will be opened in the binary mode.
